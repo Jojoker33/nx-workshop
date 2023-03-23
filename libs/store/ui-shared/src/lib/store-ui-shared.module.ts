44b844b54c8b39485cb66df989954d65299c0cc4
Sam N <sam@mirahi.io>
2023-03-23 11:38:17 +0100

    init

---
+++ libs/store/ui-shared/src/lib/store-ui-shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { StoreFeatureGameDetailModule } from '@bg-hoard/store/feature-game-detail';
+
+@NgModule({
+  imports: [CommonModule, MatToolbarModule],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
+})
+export class StoreUiSharedModule {}
